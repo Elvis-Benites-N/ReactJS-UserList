92f2db8dfe43aad8fdfc0aa19c8df83d952b5127
Elvis Benites <eebnbenites@gmail.com>
2024-11-13 17:53:49 -0500

    [config]: Configures router

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
 import React from 'react';
-import { Route, BrowserRouter as Router, Routes, Navigate } from 'react-router-dom';
+import { Route, HashRouter as Router, Routes, Navigate } from 'react-router-dom';
 import UserListPage from './components/pages/UserListPage';
 import Navbar from './components/molecules/Navbar';
+
 const App: React.FC = () => {
     return (
-        <Router future={{ v7_relativeSplatPath: true, v7_startTransition: true }}>
-            <Navbar/>
-
+        <Router>
+            <Navbar />
             <Routes>
                 <Route path="/" element={<Navigate to="/users" replace />} />
                 <Route path="/users" element={<UserListPage />} />
             </Routes>
-
-
         </Router>
     );
 };
